2c3c56eb7a16dafeba675a1bf126abc678ae80c4
paragborkar <paragborkar200@gmail.com>
2025-03-27 19:56:05 +0530

    Files Changed

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def process_image():
     return jsonify({"processed_image": img_base64})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
